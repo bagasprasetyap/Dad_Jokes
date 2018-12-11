73c6a43764b7530b24149b79d591ad5d2a700681
bagasprasetyap <bagaspp11@gmail.com>
2018-12-11 13:34:35 +0700

    versi 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  state = { question: "", answear: "" };
+
+  reload = () => {
+    var url = "https://safe-falls-22549.herokuapp.com/random_joke";
+    axios.get(url).then(props => {
+      this.setState({
+        question: props.data.setup,
+        answear: props.data.punchline
+      });
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container" style={{ marginTop: "30px" }}>
+        <div
+          className="card"
+          style={{ width: "35rem", marginLeft: "auto", marginRight: "auto" }}
+        >
+          <div className="card-body">
+            <h2 className="card-title">Dad Jokes!</h2>
+            <p className="card-text">{this.state.question}</p>
+            <p className="card-text" style={{ fontWeight: "bold" }}>
+              {this.state.answear} <i className="fas fa-grin-squint-tears" />
+            </p>
+            <a
+              href="#"
+              className="btn btn-danger"
+              onClick={() => {
+                this.reload();
+              }}
+            >
+              {`Reload `}
+              <i className="fas fa-sync-alt" />
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
